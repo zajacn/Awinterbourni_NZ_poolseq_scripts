24a8be8acb1d860b230b1603a8a5343a73d7156c
Natalia Zajac <zajac@evolbio.mpg.de>
2026-05-19 17:28:03 +0300

    Add purpose comment to mitohifi.sh
    Added a comment to indicate the purpose of the script.

--- Genome_Assembly_Organization/mitohifi.sh
+++ Genome_Assembly_Organization/mitohifi.sh
@@ -1,3 +1,4 @@
+#This script was run to search for mitochondrial genes in the nuclear genome
 echo -e "\n#Path to MitoFinder image \nexport PATH=\$PATH:$p" >> ~/.bashrc
 source ~/.bashrc
 singularity exec --bind /srv/GT/analysis/zajacn/p3203/:/srv/GT/analysis/zajacn/p3203/ mitohifi_master.sif mitohifi.py -c ../Atriophallophorus_winterbourni/EBI/GCA_013407085.1/Sequence/WholeGenomeFasta/genome.fa -f ../Clonorchis_sinensis_referencemt.fasta -g ../Clonorchis_sinensis_referencemt.gb -t 8 -a "animal" -o 21
